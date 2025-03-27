e610c4147bb0b4bce13a27d82bc52a2c5d4687df
Lukasz Zabrzenski <ZAB21951@spengergasse.at>
2025-03-27 12:17:08 +0100

    "put and patch"

---
+++ Asp_Wiederholung/src/SPG_Fachtheorie.Aufgabe3/Dtos/UpdateConfirmedCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SPG_Fachtheorie.Aufgabe3.Dtos
+{
+    public class UpdateConfirmedCommand : IValidatableObject
+    {
+        public DateTime? Confirmed { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Confirmed.HasValue)
+            {
+                // Check if the confirmed date is within 1 minute in the future
+                if (Confirmed.Value > DateTime.UtcNow.AddMinutes(1))
+                {
+                    yield return new ValidationResult(
+                        "Confirmed date cannot be more than 1 minute in the future",
+                        new[] { nameof(Confirmed) }
+                    );
+                }
+            }
+        }
+    }
+}
