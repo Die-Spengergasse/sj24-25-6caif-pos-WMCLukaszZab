f87492f73bfb203a002e93cbf0a469dbcc3b2ef6
Lukasz Zabrzenski <ZAB21951@spengergasse.at>
2025-04-04 18:22:59 +0200

    "paymentservice prog"

--- Asp_Wiederholung/src/SPG_Fachtheorie.Aufgabe3/Dtos/PaymentDto.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SPG_Fachtheorie.Aufgabe3.Dtos
-{
-    public record PaymentDto(int id, string employeeFirstName,
-        string employeeLastName, int cashDeskNumber, 
-        string paymentType, int totalAmount);
-}
